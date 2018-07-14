0b78ca1c87932f37c6ba526761c2480bf0f7f64c
shkumari <shkumari@zynga.com>
2018-07-15 00:48:00 +0530

    Initial commit

---
+++ app/src/main/java/com/paginationusingretrofitandrecyclerview/ApiCall/MovieApi.java
@@ -0,0 +1,20 @@
+package com.paginationusingretrofitandrecyclerview.ApiCall;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MovieApi {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
